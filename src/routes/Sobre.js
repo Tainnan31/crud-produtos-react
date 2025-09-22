ec3d7ec72910dce3c5e31657a83ac1934a31953d
Tainnan31 <134459552+Tainnan31@users.noreply.github.com>
2025-09-22 20:16:05 -0300

    Create Sobre.js

---
+++ src/routes/Sobre.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Sobre() {
+  return (
+    <div>
+      <h2>Sobre o Aplicativo</h2>
+      <p>Este é um CRUD de produtos desenvolvido em React com navegação SPA.</p>
+    </div>
+  );
+}
+
+export default Sobre;
