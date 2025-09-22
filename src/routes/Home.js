e42ace4acd73515b88d619b2228dae4dadac81b0
Tainnan31 <134459552+Tainnan31@users.noreply.github.com>
2025-09-22 20:15:23 -0300

    Create Home.js

---
+++ src/routes/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProdutoList from '../components/ProdutoList';
+
+function Home() {
+  return (
+    <div>
+      <h2 className="mb-4">Produtos</h2>
+      <ProdutoList />
+    </div>
+  );
+}
+
+export default Home;
